89d1a53b1e38d7affc09bb543e9ba59774044197
bookslayer10 <93895819+bookslayer10@users.noreply.github.com>
2023-10-31 13:24:43 -0700

    Formatted everything

--- src/TileEntity.java
+++ src/TileEntity.java
@@ -2,7 +2,7 @@
 public class TileEntity extends Entity {
 	private boolean collision;
 	private Game game;
-	
+
 	// 64 by 64
 	public TileEntity(Game game, String filename, int x, int y) {
 		super(filename, x, y);
@@ -11,6 +11,6 @@ public class TileEntity extends Entity {
 
 	public void collidedWith(Entity other) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
