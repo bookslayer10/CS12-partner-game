1a760ecfa8bf119890a0e74231f25074cf452a93
LiamEvans8 <146034901+LiamEvans8@users.noreply.github.com>
2023-10-31 12:38:09 -0700

    Add files via upload

---
+++ src/FileInput.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileInput {
+
+     public static String[] getFileContents(String fileName){
+    	 String [] content = null;  
+    	 try {
+    		// Read the lines from the file and collect them into a list
+             List<String> lines = Files.lines(Paths.get(fileName))
+                     .collect(Collectors.toList());
+
+             // copy the lines from the list into a 1D array
+             content = lines.toArray(new String[0]);
+
+             
+         } catch (IOException e) {
+        	 System.out.println("File Read Error");
+             e.printStackTrace();
+         } // try-catch
+    	 
+        return content;
+
+     } // getFileContents
+
+
+}  // FileInput
