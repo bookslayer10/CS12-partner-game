b563c45f8f0837b721403ef3bdada53b118d58f6
bookslayer10 <93895819+bookslayer10@users.noreply.github.com>
2023-11-01 19:19:33 -0700

    Add files via upload

---
+++ src/Entity.java
@@ -0,0 +1,140 @@
+/* Entity.java
+ * An entity is any object that appears in the game.
+ * It is responsible for resolving collisions and movement.
+ */
+
+import java.awt.*;
+
+public abstract class Entity {
+
+	// Java Note: the visibility modifier "protected"
+	// allows the variable to be seen by this class,
+	// any classes in the same package, and any subclasses
+	// "private" - this class only
+	// "public" - any class can see it
+
+	protected double x; // current x location
+	protected double y; // current y location
+	protected Sprite sprite; // this entity's sprite
+	protected double dx = 0; // horizontal speed (px/s) + -> right
+	protected double dy = 0; // vertical speed (px/s) + -> down
+	
+	protected double tx;
+	protected double ty;
+	
+	static private final long TURN_LENGTH = 1000;
+	
+	private Rectangle me = new Rectangle(); // bounding rectangle of
+											// this entity
+	private Rectangle him = new Rectangle(); // bounding rect. of other
+												// entities
+
+	/*
+	 * Constructor input: reference to the image for this entity, initial x and y
+	 * location to be drawn at
+	 */
+	public Entity(String r, int newX, int newY) {
+		x = newX;
+		y = newY;
+		sprite = (SpriteStore.get()).getSprite(r);
+	} // constructor
+
+	/*
+	 * move input: delta - the amount of time passed in ms output: none purpose:
+	 * after a certain amout of time has passed, update the location
+	 */
+	public void move(long delta) {
+		
+		x += (delta * dx) / TURN_LENGTH;
+		if((dx > 0 && x > tx) || (dx < 0 && x < tx)) {
+			x = tx;
+			dx = 0;
+			
+		} // if
+		
+		y += (delta * dy) / TURN_LENGTH;
+		if((dy > 0 && y > ty) || (dy < 0 && y < ty)) {
+			y = ty;
+			dy = 0;
+		} // if
+		
+	} // move
+	
+	// calculates the velocity of dx and dy based on target x and y
+	public void calculateMove(double tx, double ty) {
+		this.tx = tx;
+		this.ty = ty;
+		
+		dx = tx - x;
+		dy = ty - y;
+		
+		//System.out.println("dx " + dx);
+		//System.out.println("dy " + dy);
+	}
+
+	// get and set velocities
+	public void setHorizontalMovement(double newDX) {
+		dx = newDX;
+	} // setHorizontalMovement
+
+	public void setVerticalMovement(double newDY) {
+		dy = newDY;
+	} // setVerticalMovement
+
+	public double getHorizontalMovement() {
+		return dx;
+	} // getHorizontalMovement
+
+	public double getVerticalMovement() {
+		return dy;
+	} // getVerticalMovement
+
+	public void setSprite(Sprite sprite) {
+		this.sprite = sprite;
+	}
+
+	// get position
+	public int getX() {
+		return (int) x;
+	} // getX
+
+	public int getY() {
+		return (int) y;
+	} // getY
+	
+	public boolean getIsMoving() {
+		return dx != 0 || dy != 0;
+	}
+	
+	/*
+	 * Draw this entity to the graphics object provided at (x,y)
+	 */
+	public void draw(Graphics g) {
+		sprite.draw(g, (int) x, (int) y);
+	} // draw
+
+	/*
+	 * Do the logic associated with this entity. This method will be called
+	 * periodically based on game events.
+	 */
+	public void doLogic() {
+	}
+
+	/*
+	 * collidesWith input: the other entity to check collision against output: true
+	 * if entities collide purpose: check if this entity collides with the other.
+	 */
+	public boolean collidesWith(Entity other) {
+		me.setBounds((int) x, (int) y, sprite.getWidth(), sprite.getHeight());
+		him.setBounds(other.getX(), other.getY(), other.sprite.getWidth(), other.sprite.getHeight());
+		return me.intersects(him);
+	} // collidesWith
+
+	/*
+	 * collidedWith input: the entity with which this has collided purpose:
+	 * notification that this entity collided with another Note: abstract methods
+	 * must be implemented by any class that extends this class
+	 */
+	public abstract void collidedWith(Entity other);
+
+} // Entity class
