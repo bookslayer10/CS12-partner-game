8849ed19e1b77601bbd813f646b3e6d7255aaf87
bookslayer10 <93895819+bookslayer10@users.noreply.github.com>
2023-11-16 13:30:01 -0800

    comments

--- src/MeleeEntity.java
+++ src/MeleeEntity.java
@@ -5,9 +5,11 @@ public class MeleeEntity extends EnemyEntity {
 	public MeleeEntity(Game g, String r, int newX, int newY) {
 		super(g, r, newX, newY, 2);
 	} // constructor
-
+	
+	// if the melee entity gets damaged during its turn, switch the sprite list to the broken sprites
 	public void move(long delta) {
 		
+		// only activates a single time
 		if(getHealth() < 2 && !isDamaged) {
 			isDamaged = true;
 			
@@ -24,6 +26,7 @@ public class MeleeEntity extends EnemyEntity {
 		super.move(delta);
 	} // move
 	
+	// if melee entity runs into another melee entity, 
 	public void collidedWith(Entity other) {
 		if(other instanceof MeleeEntity) {
 			game.removeEntity(this);
